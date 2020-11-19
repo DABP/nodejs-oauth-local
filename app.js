e8feb6ea31188e4ee93150bf6916366780767709
David <yonghoon95@gmail.com>
2020-11-19 17:22:31 +0900

    fix: Fix all errors

--- app.js
+++ app.js
@@ -3,16 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var loginRouter = require('./router/login');
+var loginRouter = require('./routes/login');
+var session = require('express-session');
 
 var app = express();
-
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "pyh",
+    cookie: {
+        httpOnly: true,
+        secure: false
+    }
+}));
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,18 +39,18 @@ app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy({
+    usernameField: 'userid',
+    passwordField: 'password',
+    passReqToCallback: true
+}, function(req, userid, password, done) {
+    if (userid == 'hello' && password == 'world') {
+        var user = {
+            'userid': 'hello',
+            'email': 'hello@world.com'
+        };
+        return done(null, user);
+    } else {
+        return done(null, false);
+    }
+}));
+
+passport.serializeUser(function(user, done) {
+    console.log('serialize');
+    console.log(user);
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    //findById(id, function (err, user) {
+    console.log('deserialize');
+    done(null, user);
+    //});
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Oauth Login' });
+    res.render('login', { title: 'Oauth Login' });
+});
+router.post('/', passport.authenticate('local', { failureRedirect: '/login/fail', failureFlash: true }),
+    function(req, res) {
+        res.redirect('/login/success');
+    });
+router.get('/success', function(req, res) {
+    console.log('success');
+    console.log(req.user);
+    res.send(req.user);
 });
 
+router.get('/fail', function(req, res, next) {
+    console.log('fail');
+    res.send('login fail');
+});
 module.exports = router;
